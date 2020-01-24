1d6a4b0e532752aae393423741dfed054cd43902
darcipons <darcimarie33@gmail.com>
2020-01-23 22:50:19 -0500

    created navbar

--- client/src/components/Navigation/Navbar.js
+++ client/src/components/Navigation/Navbar.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { Nav } from 'react-bootstrap';
 import { Link } from "react-router-dom";
+import logo from './logo.png'
 import './Navbar.css'
 
 const Navbar = () => {
 
   return (
     <div>
+     
       <Nav className="justify-content-end" id='nav-bar' activeKey="/home">
+      <div className='navbar_logo'>
+        <img src= {logo} alt='revanix logo' ></img>
+      </div>
         <div className="nav_item">
         <Nav.Item>
           <Link className="text" to={'/'}>Home</Link>
@@ -22,6 +27,7 @@ const Navbar = () => {
         <Nav.Item>
           <Link className="text" to={'/repairs'}>Equipment Repairs</Link>
         </Nav.Item>
+      
         </div>
         <div className="nav_item">
         <Nav.Item>
@@ -31,6 +37,8 @@ const Navbar = () => {
       </Nav>
     </div>
   )
+
 }
 
 export default Navbar 
+
