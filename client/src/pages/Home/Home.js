b942421d6815a1880ef9e9112a7a075ebe9d80dc
darcipons <darcimarie33@gmail.com>
2020-01-24 13:32:05 -0500

    background image placed in center

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import './Home.css'
 
 const Home = () => {
   return (
-    <h1>Home</h1>
+    <>
+    <div className='home_main_image'>
+      <div>Certified Medical Equipment Repairs</div>
+      <div>Located in Florida and California</div>
+      <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </div>
+    </div>
+    <div>
+      <div className='home_title'>What We Do</div>
+   
+    </div>
+    <div className='services_text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Volutpat est velit egestas dui id ornare arcu. Dictum varius duis at consectetur lorem donec. Ac tortor vitae purus faucibus. Volutpat est velit egestas dui id ornare arcu. Dictum varius duis at consectetur lorem donec. Ac tortor vitae purus faucibus.
+    </div>
+    </>
   )
 }
 
