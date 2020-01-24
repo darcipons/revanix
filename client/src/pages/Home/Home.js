edf568191db2152713d9b7ee991b2fe5cbe0c813
darcipons <darcimarie33@gmail.com>
2020-01-24 15:16:35 -0500

    added services with icon, title, description

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -5,16 +5,34 @@ const Home = () => {
   return (
     <>
     <div className='home_main_image'>
-      <div>Certified Medical Equipment Repairs</div>
-      <div>Located in Florida and California</div>
-      <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      <div className='home_intro_text'>
+        <div className='home_intro_title'>Certified Medical Equipment Repairs</div>
+        <div className='home_intro_location'>Located in Florida and California</div>
+        <div className='home_intro_description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </div>
       </div>
     </div>
     <div>
       <div className='home_title'>What We Do</div>
-   
     </div>
     <div className='services_text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Volutpat est velit egestas dui id ornare arcu. Dictum varius duis at consectetur lorem donec. Ac tortor vitae purus faucibus. Volutpat est velit egestas dui id ornare arcu. Dictum varius duis at consectetur lorem donec. Ac tortor vitae purus faucibus.
+    </div>
+    <div className='services_container'>
+      <div className='services_icon1'>
+        <i class="las la-shipping-fast la-5x"></i>
+      </div>
+      <div className='services_title1'>Ship Your Equipment</div>
+      <div className='services_description1'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Velit sed ullamcorper morbi tincidunt. Neque sodales ut etiam sit amet nisl.</div>
+      <div className='services_icon2'>
+        <i class="las la-tools la-5x"></i>
+      </div>
+      <div className='services_title2'>Equipment Repair</div>
+      <div className='services_description2'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Velit sed ullamcorper morbi tincidunt. Neque sodales ut etiam sit amet nisl.</div>
+      <div className='services_icon3'>
+       <i class="las la-shipping-fast la-5x"></i>
+      </div>
+      <div className='services_title3'>Receive Your Equipment</div>
+      <div className='services_description3'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Velit sed ullamcorper morbi tincidunt. Neque sodales ut etiam sit amet nisl.</div>
     </div>
     </>
   )
