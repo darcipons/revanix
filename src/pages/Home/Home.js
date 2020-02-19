f5afc5f136fb793964803497448d35a88232a53c
darcipons <darcimarie33@gmail.com>
2020-02-19 17:24:16 -0500

    why choose us has purple squares and icons on top

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -26,48 +26,45 @@ const Home = () => {
       </div>
     </div>
 
-    <div className='icon_container'>
-      <div>
-        <img src={Touch} alt='award icon'className='services_icon'></img>
-      </div>
-      <div>
-        <img src={Award} alt='award icon'className='services_icon'></img>
-      </div>
-      <div>
-        <img src={Tv} alt='award icon'className='services_icon'></img>
-      </div>
-    </div>
+    <div className='icon_services_container'>
 
-    <div className='services_container'>
-
-      <div className='services_sub_container2'>
-        
-        <div className='services_title2'>WHY CHOOSE US</div>
-        <div className='services_description2'>
-          <ul>
-            <li>Worldwise excellent repair services</li>
-            <li>Professionally trained technicians</li>
-            <li>Free loaners</li>
-            <li>Free shipping</li>
-            <li>Competitive pricing</li>
-            <li>24/7 real-time access to repairs</li>
-          </ul>
+      <div className='icon_container'>
+        <div>
+          <img src={Touch} alt='award icon'className='services_icon'></img>
+        </div>
+        <div>
+          <img src={Award} alt='award icon'className='services_icon'></img>
+        </div>
+        <div>
+          <img src={Tv} alt='award icon'className='services_icon'></img>
         </div>
       </div>
 
-      <div className='services_sub_container3'>
-        <div className='services_title3'>CERTIFICATIONS</div>
-        <div className='services_description3'>
-          <ul>
-            <li>Revanix Biomedical is ISO 9001:2015 and ISO 13485:2016 certified medical device repair company.</li>
-            <li>We are committed to your satisfaction! </li>
-          </ul>
+      <div className='services_container'>
+        <div className='services_sub_container1'>
+          <div className='services_title1'>WHY CHOOSE US</div>
+          <div className='services_description1'>
+              <div>Worldwise excellent repair services</div>
+              <div>Professionally trained technicians</div>
+              <div>Free loaners</div>
+              <div>Free shipping</div>
+              <div>Competitive pricing</div>
+              <div>24/7 real-time access to repairs</div>
+          </div>
         </div>
-      </div>
 
-      <div className='services_sub_container4'>
-          <div className='services_title4'>OUR MISSION</div>
-          <div className='services_description4'>We are committed to providing superior customer service by exceeding customers expectations and serving them with unparalleled honesty, integrity and quality.</div>
+        <div className='services_sub_container2'>
+          <div className='services_title2'>CERTIFICATIONS</div>
+          <div className='services_description2'>
+            <div className='services_description21'>Revanix Biomedical is ISO 9001:2015 and ISO 13485:2016 certified medical device repair company.</div>
+            <div className='services_description22'>We are committed to your satisfaction! </div>
+          </div>
+        </div>
+
+        <div className='services_sub_container3'>
+            <div className='services_title3'>OUR MISSION</div>
+            <div className='services_description3'>We are committed to providing superior customer service by exceeding customers expectations and serving them with unparalleled honesty, integrity and quality.</div>
+        </div>
       </div>
     </div>
 
