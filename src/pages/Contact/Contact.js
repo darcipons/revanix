06b8fff0fea5302aee840c05839e9d09eb3d1c02
darcipons <darcimarie33@gmail.com>
2020-03-01 16:57:15 -0500

    added header and contact form

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import ContactUs from '../../components/ContactUs/ContactUs'
+import './Contact.css'
 
 const Contact = () => {
   return (
-    <h1>Contact</h1>
+    <div>
+    
+        <div className='contact_title_container'>
+          <div className='contact_title'>Contact Us</div>
+          <div className='contact_description'>The World Leader in Biomedical and Surgical Repairs</div>
+        </div>
+    
+      <div className='contactus'>
+      <ContactUs />
+      </div>
+    </div>
   )
 }
 
