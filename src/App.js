82dc72f86b880c78370161b89a5a257623bec197
darcipons <darcimarie33@gmail.com>
2020-03-19 22:03:11 -0400

    added more instrument pages

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ import Footer from './components/Footer/Footer'
 import SideDrawer from './components/SideDrawer/SideDrawer'
 import FlexibleEndoscope from './equipment/FlexibleEndoscope/FlexibleEndoscope'
 import RigidEndoscope from './equipment/RigidEndoscope/RigidEndoscope'
+import PowerEquip from './equipment/PowerEquip/PowerEquip'
+import UltrasoundProbe from './equipment/UltrasoundProbe/UltrasoundProbe'
+import Instrument from './equipment/Instrument/Instrument'
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -34,6 +37,9 @@ class App extends Component {
           <Route path='/Contact Us' component={Contact}/> 
           <Route path="/Flexible Endoscope" component={FlexibleEndoscope} />
           <Route path='/Rigid Endoscope' component={RigidEndoscope}/>
+          <Route path='/Power Equipment' component={PowerEquip}/>
+          <Route path='/Ultrasound Probe' component={UltrasoundProbe}/>
+          <Route path='/Instrument' component={Instrument}/>
         </div>
         <Footer/>
       </React.Fragment>
