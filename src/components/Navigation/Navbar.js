b082ed03fbdf71e3fc931cede5c0c39da78858f1
darcipons <darcimarie33@gmail.com>
2020-06-22 16:39:52 -0400

    react bootstrap nav bar

--- src/components/Navigation/Navbar.js
+++ src/components/Navigation/Navbar.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
-import DrawerToggleButton from '../SideDrawer/DrawerToggleButton';
+import { Navbar, Nav, NavDropdown, Dropdown, DropdownButton, ButtonGroup }from 'react-bootstrap/'
+// import { Link } from 'react-router-dom'
+// import DrawerToggleButton from '../SideDrawer/DrawerToggleButton';
 import DropDown from '../NavbarDropDown/NavbarDropDown'
-import Locations from '../../components/LocationsDropDown/LocationsDropDown'
+// import Locations from '../../components/LocationsDropDown/LocationsDropDown'
 import logo from './logo.png'
 import './Navbar.css'
 
-const Navbar = props => {
+const Navbarr = props => {
 
   return (
     <div>
-      <header className='toolbar'>
+      {/* <header className='toolbar'>
         <nav className='toolbar_nav'>
           <div className='toolbar_toggle_button'>
             <DrawerToggleButton click={props.drawerClickHandler}/>
@@ -37,10 +38,52 @@ const Navbar = props => {
             
           </div>
         </nav>
-      </header>
+      </header> */}
+
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="/">
+          <img src={logo} alt='reavanix logo' width='270' height='120' className="d-inline-block align-top"/>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="/About Us">About Us</Nav.Link>
+            <Nav.Link href="/Equipment Sale">Equipment Sale/Purchase</Nav.Link>
+            <Nav.Link href="/Education">Education</Nav.Link>
+            <Nav.Link href="/Career">Career</Nav.Link>
+            <Nav.Link href="/Contact Us">Contact Us</Nav.Link>
+            <Nav.Link>
+              <Dropdown />
+            </Nav.Link>
+            <NavDropdown title="Equipment Repairs" id="basic-nav-dropdown">
+              <DropdownButton key="right" id="dropdown-button-drop-right" drop="right" title="Surgical Repairs" as={ButtonGroup} variant="light">
+                <Dropdown.Item href="/Flexible Endoscope">Flexible Endoscope</Dropdown.Item>
+                <Dropdown.Item href="/Rigid Endoscope">Rigid Endoscope</Dropdown.Item>
+                <Dropdown.Item href="/Power Equipment">Power Equipment</Dropdown.Item>
+                <Dropdown.Item href="/Video Equipment">Video Equipment</Dropdown.Item>
+                <Dropdown.Item href="/Ultrasound Probe">Ultrasound Probe</Dropdown.Item>
+              </DropdownButton>
+
+              <DropdownButton key="right" id="dropdown-button-drop-right" drop="right" title="Patient Repairs" as={ButtonGroup} variant="light">
+                <Dropdown.Item href="/Patient Monitoring">Patient Monitoring</Dropdown.Item>
+                <Dropdown.Item href="/Patient Cables">Patient Cables</Dropdown.Item>
+                <Dropdown.Item href="/Parts">Parts</Dropdown.Item>
+                <Dropdown.Item href="/Infusion Pumps">Infusion Pumps</Dropdown.Item>
+                <Dropdown.Item href="/Blenders and Regulators">Blenders and Regulators</Dropdown.Item>
+              </DropdownButton>
+{/*            
+              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item> */}
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+
     </div>
   )
 }
 
-export default Navbar 
+export default Navbarr 
 
