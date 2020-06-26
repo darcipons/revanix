584bdc479c442aeb5aea6ef26487153e3ee4aa85
darcipons <darcimarie33@gmail.com>
2020-06-26 15:46:06 -0400

    added bullet points to about us page

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Office from './office-group.jpg'
-import Commitment from '../../components/Commitment/Commitment'
+import Bullet from './bullet.png'
 import './About.css'
 
 
@@ -20,7 +20,26 @@ const About = () => {
         <div className='about_us_description_container'>
           <div className='about_us_title'>Who We Are</div>
           <div className='about_us_description'>Revanix Biomedical is a privately held company with over 25 years of combined experience in the medical repair industry and provides services worldwide. As a leading international medical device repair company, Revanix Biomedical is dedicated to provide the highest quality products and services in the most cost-effective manner. The entire Revanix Biomedical team has a strong commitment to excellence.</div>
-          <Commitment />
+
+          <div className='about_us_commitment'>
+            <div className='about_commitment_title'>Our commitment to you: </div>
+            <div className='about_commitment'>
+              <img src={Bullet} alt='bullet point' className='bullet_point'/>
+              <strong>Honesty </strong>- Always telling the truth during the service and repair process, never mislead. Always make recommendations by order of priority of customers so they can plan for future service needs.
+            </div>
+            <div className='about_commitment'>
+              <img src={Bullet} alt='bullet point' className='bullet_point'/>
+              <strong>Integrity</strong> - Always doing the right thing, even when no one is watching. We build trust in our relationships through honest and ethical behavior.
+            </div>
+            <div className='about_commitment'>
+              <img src={Bullet} alt='bullet point' className='bullet_point'/>
+              <strong>Quality and Excellence</strong> - Set and achieve the highest standards in everything we do by using the best possible equipment, parts and personnel.
+            </div>
+            <div className='about_commitment'>
+              <img src={Bullet} alt='bullet point' className='bullet_point'/>
+              <strong>Team Work</strong> - We work together with you to make a difference.
+            </div>
+          </div>
         </div>
       </div>
 
