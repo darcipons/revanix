95424dbf0554a780b7fd5a3b95cb24c4698e36e9
darcipons <darcimarie33@gmail.com>
2020-06-30 21:43:42 -0400

    updated instruments page

--- src/surgicalEquipment/Instruments/Instruments.js
+++ src/surgicalEquipment/Instruments/Instruments.js
@@ -7,58 +7,42 @@ function Instruments () {
     <div>
       <div className='rigid_header_container'>
         <div className='rigid_title'>Instruments</div>
-        <div className='rigid_repair_description'>Revanix Biomedical repair all rigid types of endoscopes and Specialty Scopes. And to the OEM specifications when needed.</div>
       </div>
 
       <div className='rigid_repair_services_container'>
         <div className='rigid_repair_services_title'>Instrument Services</div>
-        <div className='rigid_repair_services_description'>We use only the quality replacement components. Rest Assured our team of Technical Specialists will complete a comprehensive evaluation of damage, and perform leak & shock testing and properly replace all components. In addition, for the perfect hermetic seal of all autoclavable scopes laser welded using our YAG laser welder. </div>
+        <div className='rigid_repair_services_description'>Revanix Biomedical understands surgical instruments have a wide vary of manufacturers, many makes and models, and many parts for many purposes. Our services will demonstrate quick turnaround time and pricing that will leave the quality of repair exceptional. </div>
       </div>
 
-      <div className='rigid_center_container'>
-        <div className='rigid_center1'>
-          <img src={InstrumentPic} alt='rigid endoscope' className='rigid_image'></img>
+      <div className='instrument_center_container'>
+        <div className='instrument_img_container'>
+          <img src={InstrumentPic} alt='rigid endoscope' className='instrument_img'></img>
         </div>
 
-        <div className='rigid_center_middle'>
-            <div className='rigid_center_title'>1 Year Warranty </div>
-            <div className='rigid_center_list'>
+        <div className='instrument_middle_container'>
+            <div className='instrument_title'>Instruments We Repair </div>
+            <div className='instrumnet_list'>
               <ul>
-                <li>Quality Rod Lens Replacement</li>
-                <li>Free Evaluations And Repair Quotes</li>
-                <li>Free Overnight Shipping</li>
+                <li>General Instruments</li>
+                <li>Micro Instruments</li>
+                <li>Laproscopic Instruments</li>
+                <li>Electro-Surgical</li>
               </ul>
             </div>
         </div>
       </div>
 
-      <div className='rigid_middle_container'>
-        <div className='rigid_perform_container'>
-          <div className='rigid_perform_title'>4-10 MM Rigid Scopes</div>
-          <div className='rigid_perform_list'>
-            <ul>
-              <li>Flat Rate Repairs</li>
-              <li>CCD Component Replacement</li>
-              <li>Replace broken Rod Lenses</li>
-              <li>Replace Light Bundles</li>
-            </ul>
-          </div>
-        </div>
-
-        <div className='rigid_services_container'>
-          <div className='rigid_services_title'>Under 4mm - Specialty Rigid Endoscopes</div>
-          <div className='rigid_services_list'>
-            <ul>
-              <li>All Optic Repairs</li>
-              <li>Distal Tip</li>
-              <li>Polishing</li>
-              <li>Removal of Dents From Tubes</li>
-              <li>Refiber Up To OEM Specifications </li>
-            </ul>
-          </div>
+      
+      <div className='advan_container instrument'>
+        <div className='advan_title instrument'>Revanix Advantages </div>
+        <div className='advan_list instrument'>
+          <ul>
+            <li>Free Evaluations</li>
+            <li>Free Estimates</li>
+            <li>Quick Turnaround Time</li>
+          </ul>
         </div>
       </div>
-
   </div>
   )
 }
