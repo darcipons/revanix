225ce6e758027960373b92a96e13766b9f7ab378
darcipons <darcimarie33@gmail.com>
2020-07-02 15:27:50 -0400

    created parts page, need to fix intro description

--- src/patientEquipment/Parts/Parts.js
+++ src/patientEquipment/Parts/Parts.js
@@ -6,7 +6,20 @@ const Parts = () => {
     <div>
        <div className='rigid_header_container'>
           <div className='rigid_title'>Parts</div>
-          {/* <div className='rigid_repair_description'>Revanix Biomedical will provide a cost-effective means of servicing and repairing your ultrasound systems without the risk of downtime or lost revenue. </div> */}
+        </div>
+
+        <div className='rigid_repair_services_container'>
+          <div className='rigid_repair_services_title'>Parts Repair Services</div>
+          <div className='rigid_repair_services_description'>Revanix Biomedical has the capability to only repair your equipment but we offer to save customers from the bundle of high costs. Revanix can provide the parts you need for your everyday use with the savings you demand. </div>
+        </div>
+
+        <div className='parts_container'>
+          <ul>
+            <li>Alaris 8100 and 8015</li>
+            <li>NIBP - Subassembly</li>
+            <li>Interior and Exterior Parts</li>
+            <li>And much more</li>
+          </ul>
         </div>
     </div>
    );
