f8aefe01c7494d99edbc573df67475b9f87a3d4c
darcipons <darcimarie33@gmail.com>
2020-07-02 18:01:39 -0400

    created fiber optic and footpedal

--- src/patientEquipment/Footpedal/Footpedal.js
+++ src/patientEquipment/Footpedal/Footpedal.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Foot from './foot.png'
 import './Footpedal.css'
 
 const Footpedal = () => {
@@ -6,7 +7,40 @@ const Footpedal = () => {
     <div>
        <div className='rigid_header_container'>
           <div className='rigid_title'>Footpedal</div>
-          {/* <div className='rigid_repair_description'>Revanix Biomedical will provide a cost-effective means of servicing and repairing your ultrasound systems without the risk of downtime or lost revenue. </div> */}
+        </div>
+
+        <div className='rigid_repair_services_container'>
+          <div className='rigid_repair_services_title'>Footpedal Repair Services</div>
+          <div className='rigid_repair_services_description'>Revanix Biomedical offers quality services on all types of foot pedals with a quick turnaround time. Our highly skilled technician will diagnose and repair a variety of foot switches for a flat rate price, our expertise will determine the quality of repair for your needs. </div>
+        </div>
+
+        <div className='foot_container'>
+        <div className='foot_brand_container1'>
+            <div className='foot_title'>Brands We Repair</div>
+            <div className='foot_list'>
+              <ul>
+                <li>Stryker</li>
+                <li>Valley Lab</li>
+                <li>Dyonics</li>
+                <li>and Many More</li>
+              </ul>
+            </div>
+          </div>
+
+          <div className='foot_brand_container2'>
+            <div className='foot_title'>Revanix Advantages</div>
+            <div className='foot_list'>
+              <ul>
+                <li>Free Evaluations</li>
+                <li>Free Estimates</li>
+                <li>Flat Rate Price: $495</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+        <div className='foot_img_container'>
+          <img src={Foot} alt='Foot Pedal' className='foot_img'/>
         </div>
     </div>
    );
