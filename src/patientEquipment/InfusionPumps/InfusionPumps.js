c388c34622a5896513db3f02b763f8a96a7067e4
darcipons <darcimarie33@gmail.com>
2020-07-02 15:47:40 -0400

    created infusion pumps page, imgs still need to be centered

--- src/patientEquipment/InfusionPumps/InfusionPumps.js
+++ src/patientEquipment/InfusionPumps/InfusionPumps.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Pumps1 from './pumps1.png'
+import Pumps2 from './pumps2.png'
 import './InfusionPumps.css'
 
 const InfusionPumps = () => {
@@ -6,7 +8,45 @@ const InfusionPumps = () => {
     <div>
        <div className='rigid_header_container'>
           <div className='rigid_title'>Infusion Pumps</div>
-          {/* <div className='rigid_repair_description'>Revanix Biomedical will provide a cost-effective means of servicing and repairing your ultrasound systems without the risk of downtime or lost revenue. </div> */}
+        </div>
+
+        <div className='rigid_repair_services_container'>
+          <div className='rigid_repair_services_title'>Infusion Pumps Repair Services</div>
+          <div className='rigid_repair_services_description'>Revanix Biomedical provides full service equipment maintenance and repair on a variety of infusion/ IV pumps. Our biomedical technician are trained with years of skilled training and and provide preventive maintenance and quality service. </div>
+        </div>
+
+        <div className='pumps_container'>
+          <div className='pumps_description1'>
+            <div className='pumps_title'>Brands We Repair</div>
+            <div className='pumps_list'>
+              <ul>
+                <li>Carefusion</li>
+                <li>Alaris</li>
+                <li>Baxter</li>
+                <li>BD</li>
+                <li>CME</li>
+              </ul>
+            </div>
+          </div>
+
+          <div className='pumps_img_container1'>
+            <img src={Pumps1} alt='Infusion Pumps' className='pumps_img' />
+          </div>
+
+          <div className='pumps_description2'>
+            <div className='pumps_title'>Revanix Advantages</div>
+            <div className='pumps_list'>
+              <ul>
+                <li>Flat Rate Prices</li>
+                <li>Free Evaluations And Repair Quotes</li>
+                <li>PM's</li>
+              </ul>
+            </div>
+          </div>
+
+          <div className='pumps_img_container2'>
+            <img src={Pumps2} alt='Infusion Pumps' className='pumps_img'/>
+          </div>
         </div>
     </div>
    );
