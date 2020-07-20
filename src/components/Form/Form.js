7f7314a01862b6840c4a7deccd15ab99a03e7d51
darcipons <darcimarie33@gmail.com>
2020-07-20 15:48:36 -0400

    form ready for backend

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,44 +1,77 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Form, ButtonToolbar, Button } from 'react-bootstrap'
-
+import axios from 'axios'
 import './Form.css'
 
-const FormBox = () => {
+class FormBox extends Component {
+  constructor() {
+    super()
 
-  return (
-    <>
-      
-      <Form>
-        <div className='contact_us_title'>Contact Us</div>
+    this.state = {
+      name: '',
+      email: '',
+      phone: '',
+      subject: '',
+      message: ''
+    }
+
+    this.handleChange = this.handleChange.bind(this) 
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value })
+  }
+
+  async handleSubmit(e) {
+    e.preventDefault()
+
+    const { name, email, phone, subject, message } = this.state
+
+    const form = await axios.post('/api/form', {
+      name,
+      email,
+      phone,
+      subject,
+      message
+    })
+  }
+
+  render() {
+    return (
+      <>
+        <Form onSubmit={this.handleSubmit} >
+          <div className='contact_us_title'>Contact Us</div>
+          
+          <Form.Group controlId="name" className='form_inputs'>
+            <Form.Control type="text" placeholder="Name" name="name" onChange={this.handleChange}/>
+          </Form.Group>
+
+          <Form.Group controlId="name" className='form_inputs'>
+            <Form.Control type="email" placeholder="Email" name="email" onChange={this.handleChange}/>
+          </Form.Group>
+
+          <Form.Group controlId="name" className='form_inputs'>
+            <Form.Control type="text" placeholder="Phone" name="phone" onChange={this.handleChange} />
+          </Form.Group>
+
+          <Form.Group controlId="name" className='form_inputs'>
+            <Form.Control type="text" placeholder="Subject"  name="subject" onChange={this.handleChange}/>
+          </Form.Group>
+          
+          <Form.Group controlId="textarea" className='form_inputs'>
+            <Form.Control as="textarea" rows="3"  placeholder="Message" name="message" onChange={this.handleChange}/>
+          </Form.Group>
+
+          <ButtonToolbar>
+            <Button variant="primary" size="lg"> Submit </Button>
+          </ButtonToolbar>
+
+        </Form>
         
-        <Form.Group controlId="name" className='form_inputs'>
-          <Form.Control type="text" placeholder="Name" />
-        </Form.Group>
-
-        <Form.Group controlId="name" className='form_inputs'>
-          <Form.Control type="email" placeholder="Email" />
-        </Form.Group>
-
-        <Form.Group controlId="name" className='form_inputs'>
-          <Form.Control type="email" placeholder="Phone" />
-        </Form.Group>
-
-        <Form.Group controlId="name" className='form_inputs'>
-          <Form.Control type="email" placeholder="Subject" />
-        </Form.Group>
-        
-        <Form.Group controlId="textarea" className='form_inputs'>
-          <Form.Control as="textarea" rows="3"  placeholder="Message"/>
-        </Form.Group>
-
-        <ButtonToolbar>
-          <Button variant="primary" size="lg"> Submit </Button>
-        </ButtonToolbar>
-
-      </Form>
-      
-    </>
-  )
+      </>
+    )
+  }
 }
 
 export default FormBox
